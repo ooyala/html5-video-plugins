f3fafdac1e1c2f84aba91a148b7476438f8c3bfd
Angie Israni <aisrani@ooyala.com>
2015-11-12 12:02:21 -0800

    [jest] Add infrastructure for unit testing

---
+++ tests/html5videotag-test.js
@@ -0,0 +1,8 @@
+jest.dontMock('../js/videoPlugin_html5videotag');
+
+var basicHtml5 = require('../js/videoPlugin_html5videotag');
+
+describe('basic_html5', function () {
+  it('Registers factory', function () {
+  });
+});
