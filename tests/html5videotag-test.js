7867e929e0fa71a514ad1acde7519ac8ca795f7b
Angie Israni <aisrani@ooyala.com>
2015-11-23 14:21:21 -0800

    [Tests] Update unit tests framework for new file names

--- tests/html5videotag-test.js
+++
@@ -1,8 +0,0 @@
-jest.dontMock('../js/videoPlugin_html5videotag');
-
-var basicHtml5 = require('../js/videoPlugin_html5videotag');
-
-describe('basic_html5', function () {
-  it('Registers factory', function () {
-  });
-});
