433c6cc40ef7b73200a2a840f13c65590d6d3f5b
Angie Israni <aisrani@ooyala.com>
2015-11-30 11:10:16 -0800

    [Tests] Change file paths and include expect.js

--- tests/unit/main_html5-test.js
+++ tests/unit/main_html5-test.js
@@ -1,9 +1,16 @@
-jest.dontMock('../js/main_html5');
+/*
+ * https://github.com/Automattic/expect.js
+ */
 
-OO = { Video: { plugin: function() {} } };
-var basicHtml5 = require('../js/main_html5');
+require("expect.js");
+jest.dontMock('../../js/main_html5');
+
+var pluginFactory;
+OO = { Video: { plugin: function(plugin) { pluginFactory = plugin; } } };
+var basicHtml5 = require('../../js/main_html5');
 
 describe('basic_html5', function () {
-  it('Registers factory', function () {
+  it('factory: should contain parameter \'name\'', function () {
+    expect(pluginFactory.name).to.be.ok();
   });
 });
