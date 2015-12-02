268603e40cd99476b640725d99d864d216a1dd14
Angie Israni <aisrani@ooyala.com>
2015-12-01 16:34:42 -0800

    [PBW-3498][Tests] Add complete unit tests for the factory

--- tests/unit/main_html5/factory-tests.js
+++ tests/unit/main_html5/factory-tests.js
@@ -29,9 +29,53 @@ describe('main_html5 factory tests', function () {
   });
 
   it('should be able to create an element', function(){
+    var elementWrapper = pluginFactory.create($("<div>"), "test", {}, {});
+    expect(elementWrapper).to.be.ok();
+  });
+
+  it('should create an element wrapper with provided contoller property', function(){
+    var controller = { "iAm" : "theController" };
+    var elementWrapper = pluginFactory.create($("<div>"), "test", controller, {});
+    expect(elementWrapper.controller).to.eql(controller);
+  });
+
+  it('should create element under parentElement', function(){
     var parentElement = $("<div>");
-    var newElement = pluginFactory.create(parentElement, "test", {}, {});
-    expect(newElement).to.be.ok();
+    pluginFactory.create(parentElement, "test", {}, {});
+    expect(parentElement.children().length).to.eql(1);
+  });
+
+  it('should create element of class "video"', function(){
+    var parentElement = $("<div>");
+    pluginFactory.create(parentElement, "test", {}, {});
+    var element = parentElement.children()[0];
+    expect(element.getAttribute("class")).to.eql("video");
+  });
+
+  it('should create element with given domId', function(){
+    var domId = "testId";
+    var parentElement = $("<div>");
+    pluginFactory.create(parentElement, domId, {}, {});
+    var element = parentElement.children()[0];
+    expect(element.getAttribute("id")).to.eql(domId);
+  });
+
+  it('should create element with given css', function(){
+    var css = { "visibility" : "hidden" };
+    var parentElement = $("<div>");
+    pluginFactory.create(parentElement, "test", {}, css);
+    var element = parentElement.children()[0];
+    expect(element.getAttribute("style")).to.be.ok();
+    expect(element.getAttribute("style")).to.eql("visibility: hidden;");
+  });
+
+  it('should create element with proper attributes', function(){
+    var parentElement = $("<div>");
+    pluginFactory.create(parentElement, "test", {}, {});
+    var element = parentElement.children()[0];
+    expect(element.getAttribute("preload")).to.eql("none");
+    expect(element.getAttribute("loop")).to.not.be.ok();;
+    expect(element.getAttribute("autoplay")).to.not.be.ok();;
   });
 
   it('should remove list of encodings on destroy', function(){
@@ -40,12 +84,15 @@ describe('main_html5 factory tests', function () {
   });
 
   it('should set ready to false on destroy', function(){
+    // destroy done in pervious test.  The proper way to test this would be to re-create the plugin
+    // before each test but this requires use of the require.cache function which is not available in jest.
     expect(pluginFactory.ready).to.be(false);
   });
 
   it('should not create elements after destroy is called', function(){
-    var parentElement = $("<div>");
-    var newElement = pluginFactory.create(parentElement, "test", {}, {});
-    expect(newElement).to.not.be.ok();
+    // destroy done in pervious test.  The proper way to test this would be to re-create the plugin
+    // before each test but this requires use of the require.cache function which is not available in jest.
+    var elementWrapper = pluginFactory.create($("<div>"), "test", {}, {});
+    expect(elementWrapper).to.not.be.ok();
   });
 });
