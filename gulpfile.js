50e46be10c1643fa2d76a8f2de5e08c1996fc11d
Angie Israni <aisrani@ooyala.com>
2015-12-10 16:49:28 -0800

    [PBW-3869] Restructure main_html5 folders and change build script to support structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,75 +6,44 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     buffer = require('vinyl-buffer'),
-    gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
     shell = require('gulp-shell'),
     rename = require('gulp-rename');
-    listFiles = require('file-lister');
-
-var path = {
-  originalJs: ['./js/']
-};
 
 // Build All
-gulp.task('build', ['browserify']);
-
-gulp.task('browserify', function() {
-
-  var bundleThis = function(srcArray)
-  {
-    for (index in srcArray)
-    {
-      var sourceFile = srcArray[index];
-      var b = browserify({
-        entries: sourceFile,
-        debug: false,
-      });
-
-      b.bundle()
-        .pipe(source(getFileNameFromPath(sourceFile)))
-        .pipe(buffer())
-        .pipe(gulp.dest('./build/'))
-        .pipe(uglify())
-        .pipe(rename({
-          extname: '.min.js'
-        }))
-        .pipe(gulp.dest('./build/'))
-    }
-  };
-
-  listFiles(path.originalJs, function(error, files) {
-    if (error) {
-        console.log(error);
-    } else {
-      var filteredList = files.filter(checkFileExtension.bind(this,".js"));
-      bundleThis(filteredList);
-    }});
-
-});
-
-var checkFileExtension = function(extension, fileName)
-{
-    if (!fileName || fileName.length < extension.length)
-    {
-      return false;
-    }
-
-    return (fileName.lastIndexOf(extension) == fileName.length - extension.length);
-}
-
-var getFileNameFromPath = function(path)
-{
-  var start = path.lastIndexOf('/') + 1;
-  return path.substring(start);
-}
+gulp.task('build', ['build_main_html5']);
 
 gulp.task('test', shell.task(['jest --verbose']));
 
 // Initiate a watch
 gulp.task('watch', function() {
-  gulp.watch(path.scripts, ['browserify']);
+  gulp.watch(path.scripts, ['build']);
 });
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['build']);
+
+gulp.task('build_main_html5', function() {
+  var sourceFile = './src/main/js/main_html5.js';
+  var b = browserify({
+    entries: sourceFile,
+    debug: false,
+  });
+
+  b.bundle()
+    .pipe(source(getFileNameFromPath(sourceFile)))
+    .pipe(buffer())
+    .pipe(gulp.dest('./build/'))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./build/'))
+});
+
+// helper functions
+var getFileNameFromPath = function(path)
+{
+  var start = path.lastIndexOf('/') + 1;
+  return path.substring(start);
+}
