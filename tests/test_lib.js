319e959f00f16804bf1fb8bce581ce6af747d3d3
Angie Israni <aisrani@ooyala.com>
2015-12-14 14:30:31 -0800

    [html5-common] Add html5-common as a submodule and use it in tests

---
+++ tests/test_lib.js
@@ -0,0 +1,27 @@
+jest.dontMock('underscore');
+jest.dontMock('jquery');
+
+global.OO = { publicApi: {}, platform: 'MacIntel', os: {}, browser: { version:1, webkit:true }, TEST_TEST_TEST: true};
+
+// The function setTimeout from jsdom is not working, this overwrites the function with the function defined by node
+global.window.setTimeout = setTimeout;
+global.navigator = window.navigator;
+
+global.expect = require('expect.js');
+
+// a wrapper domparser simulating Mozilla DOMParser in the browser:
+window.DOMParser = function() {};
+
+require("../html5-common/js/utils/InitModules/InitOOUnderscore.js");
+
+OO._.extend(window.DOMParser.prototype, {
+  parseFromString: function(data, type) {
+    return jsdom.jsdom(data, jsdom.level(3, 'core'));
+  }
+});
+
+// In a browser environment, all of the properties of "window" (like navigator) are in the global scope:
+OO._.extend(global, window);
+
+// This needs to come after the extend(global, window) line above otherwise $ gets set to undefined.
+require("../html5-common/js/utils/InitModules/InitOOJQuery.js");
