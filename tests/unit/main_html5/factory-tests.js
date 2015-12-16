207daf77bc0581561a79c5691cc8a772692b070a
Angie Israni <aisrani@ooyala.com>
2015-12-16 15:15:34 -0800

    [Tests] Fix unit tests after addition of CAN_PLAY event

--- tests/unit/main_html5/factory-tests.js
+++ tests/unit/main_html5/factory-tests.js
@@ -11,6 +11,9 @@ describe('main_html5 factory tests', function () {
   jest.dontMock('../../../src/main/js/main_html5');
   require('../../../src/main/js/main_html5');
 
+  var vtc = { EVENTS: { CAN_PLAY: "can_play" },
+                        notify: function(){} };
+
   it('should contain parameter \'name\'', function () {
     expect(pluginFactory.name).to.be.ok();
   });
@@ -26,25 +29,25 @@ describe('main_html5 factory tests', function () {
   });
 
   it('should be able to create an element', function(){
-    var elementWrapper = pluginFactory.create($("<div>"), "test", {}, {});
+    var elementWrapper = pluginFactory.create($("<div>"), "test", vtc, {});
     expect(elementWrapper).to.be.ok();
   });
 
   it('should create an element wrapper with provided contoller property', function(){
-    var controller = { "iAm" : "theController" };
+    var controller = { "iAm" : "theController", EVENTS: { CAN_PLAY: "canplay" }, notify: function(){} };
     var elementWrapper = pluginFactory.create($("<div>"), "test", controller, {});
     expect(elementWrapper.controller).to.eql(controller);
   });
 
   it('should create element under parentElement', function(){
     var parentElement = $("<div>");
-    pluginFactory.create(parentElement, "test", {}, {});
+    pluginFactory.create(parentElement, "test", vtc, {});
     expect(parentElement.children()).to.have.length(1);
   });
 
   it('should create element of class "video"', function(){
     var parentElement = $("<div>");
-    pluginFactory.create(parentElement, "test", {}, {});
+    pluginFactory.create(parentElement, "test", vtc, {});
     var element = parentElement.children()[0];
     expect(element.getAttribute("class")).to.eql("video");
   });
@@ -52,7 +55,7 @@ describe('main_html5 factory tests', function () {
   it('should create element with given domId', function(){
     var domId = "testId";
     var parentElement = $("<div>");
-    pluginFactory.create(parentElement, domId, {}, {});
+    pluginFactory.create(parentElement, domId, vtc, {});
     var element = parentElement.children()[0];
     expect(element.getAttribute("id")).to.eql(domId);
   });
@@ -60,7 +63,7 @@ describe('main_html5 factory tests', function () {
   it('should create element with given css', function(){
     var css = { "visibility" : "hidden" };
     var parentElement = $("<div>");
-    pluginFactory.create(parentElement, "test", {}, css);
+    pluginFactory.create(parentElement, "test", vtc, css);
     var element = parentElement.children()[0];
     expect(element.getAttribute("style")).to.be.ok();
     expect(element.getAttribute("style")).to.contain("visibility");
@@ -69,7 +72,7 @@ describe('main_html5 factory tests', function () {
 
   it('should create element with proper attributes', function(){
     var parentElement = $("<div>");
-    pluginFactory.create(parentElement, "test", {}, {});
+    pluginFactory.create(parentElement, "test", vtc, {});
     var element = parentElement.children()[0];
     expect(element.getAttribute("preload")).to.eql("none");
     expect(element.getAttribute("loop")).to.not.be.ok();;
@@ -90,7 +93,7 @@ describe('main_html5 factory tests', function () {
   it('should not create elements after destroy is called', function(){
     // destroy done in pervious test.  The proper way to test this would be to re-create the plugin
     // before each test but this requires use of the require.cache function which is not available in jest.
-    var elementWrapper = pluginFactory.create($("<div>"), "test", {}, {});
+    var elementWrapper = pluginFactory.create($("<div>"), "test", vtc, {});
     expect(elementWrapper).to.not.be.ok();
   });
 });
