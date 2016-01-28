9d45d1ca831fbc2779454dea018a8fc61d28bd0c
Dimitry Nasledov <dima@ooyala.com>
2016-01-28 11:52:55 -0800

    [PBW-4244] Disable "screenLogoImage" AKA watermark

--- src/bit/js/bit_wrapper.js
+++ src/bit/js/bit_wrapper.js
@@ -131,6 +131,7 @@ require("../../../html5-common/js/utils/environment.js");
 
     var conf = {
       key: this.controller.PLUGIN_MAGIC,
+      screeenLogoImage: "",
       style: {
         width: '100%',
         height: '100%',
