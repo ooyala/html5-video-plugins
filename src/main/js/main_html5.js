ad020cb319cda743f90b29ff12574fcc0eddd0b7
bwong <bwong@ooyala.com>
2016-04-11 11:42:10 -0700

    [PBW-4534] changes based on code review

--- src/main/js/main_html5.js
+++ src/main/js/main_html5.js
@@ -1195,10 +1195,10 @@ require("../../../html5-common/js/utils/environment.js");
                       _video.currentTime);
           _.defer(raiseEndedEvent);
         }
-        if(OO.isSafari && !OO.isIos && !_video.ended && Math.round(_video.currentTime) === Math.round(_video.duration))
+        else if (OO.isSafari && !OO.isIos && !_video.ended && Math.round(_video.currentTime) === Math.round(_video.duration))
         {
           this.controller.notify(this.controller.EVENTS.SEEKED);
-          _.defer(raiseEndedEvent);
+          this.controller.notify(this.controller.EVENTS.ENDED);
         }
       }
     }, this);
