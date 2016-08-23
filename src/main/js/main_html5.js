e2b52ea9fb870f6afde8221a431882fd26d6538c
bwong <bwong@ooyala.com>
2016-08-23 10:26:31 -0700

    [PBW-4456] Removed duplicate videoEnded=false that was confusing player on replay. videoEnded=false remain in exeuctePlay where it belongs.

--- src/main/js/main_html5.js
+++ src/main/js/main_html5.js
@@ -634,7 +634,6 @@ require("../../../html5-common/js/utils/environment.js");
       stopUnderflowWatcher();
       _currentUrl = _video.src;
       firstPlay = true;
-      videoEnded = false;
       isSeeking = false;
     }, this);
 
